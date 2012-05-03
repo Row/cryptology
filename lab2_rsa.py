d5993645d365f4a95e5b668d101bad27f3926aa1
Jon Borglund <jon.borglund@gmail.com>
2012-05-03 15:23:07 +0200

    Prime test

--- lab2_rsa.py
+++ lab2_rsa.py
@@ -1,3 +1,4 @@
+import random
 # Schneier 1996, p. 224. (wikipedia)
 def modpow(b, e, m):
     result = 1
@@ -10,28 +11,51 @@ def modpow(b, e, m):
 
 assert modpow(2,1,2) == 0, "Not equal"
 assert modpow(4,13,497) == 445, "Not equal"
+assert modpow(2,21701,2) == 0, "Not equal"
+
+def get_rand():
+    return random.randint(pow(2,32), pow(2,512))
+
 
 # Constants
 COMPOSITE = 0
-PROBLABLY_PRIME = 1
-
-# Miller–Rabin primality test
+PROBABLY_PRIME = 1
+    
+# Miller Rabin primality test
 # Input: n > 3, an odd integer to be tested for primality;
 # Input: k, a parameter that determines the accuracy of the test
 # Output: composite if n is composite, otherwise probably prime
 def primality_test(n, k):
+    s = 0
+    d = n - 1
+    while(d & 1): 
+        d = d >> 1
+        s += 1
     for i in range(k):
-        
+        a = random.randint(2, n - 2)
+        x = modpow(a,d,n)
+        bo = 0
+        if (x != 1 and x != n - 1):
+            for r in range(1, s):
+                x = modpow(x, 2, n)
+                if x == 1:
+                    return COMPOSITE
+                bo = x == n - 1 
+                if bo:
+                    break
+            if bo:
+                continue
+            return COMPOSITE
+    return PROBABLY_PRIME
 
-write n − 1 as 2s·d with d odd by factoring powers of 2 from n − 1
+print primality_test(7, 2) == PROBABLY_PRIME
+print primality_test(12, 2) == COMPOSITE
+print primality_test(pow(2,512) - 1, 2) == COMPOSITE
 
-LOOP: repeat k times:
-   pick a random integer a in the range [2, n − 2]
-   x ← ad mod n
-   if x = 1 or x = n − 1 then do next LOOP
-   for r = 1 .. s − 1
-      x ← x2 mod n
-      if x = 1 then return composite
-      if x = n − 1 then do next LOOP
-   return composite
-return probably prime
+prime = 4
+while(COMPOSITE != primality_test(prime, 10)):
+    print prime
+    prime = get_rand()
+
+print prime
+
